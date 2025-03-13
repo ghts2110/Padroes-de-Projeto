b12367b37c401c6d8efc94d1d49ef6abaa9ec9b7
ghts2110 <gabrieltorres2110@gmail.com>
2025-03-13 16:02:47 -0300

    connect to railway

---
+++ src/server/db.js
@@ -0,0 +1,25 @@
+async function connect() {
+    if(global.connection){
+        return global.connection.connect();
+    }
+
+    const { Pool } = require("pg");
+    const pool = new Pool({
+        connectionString: process.env.CONECTION_STRING
+    });
+    
+    console.log(process.env.CONECTION_STRING);
+
+
+    const client = await pool.connect();
+    console.log("criou o pool de coneccao");
+
+    const res = await client.query("select now()");
+    console.log(res.rows[0]);
+    client.release(); 
+
+    global.connection = pool;
+    return pool.connect();
+}
+
+connect();
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+require("dotenv").config();
+
+const db = require("./db");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const indexRoutes = require('/Users/gabriel/Desktop/calendar/src/server/routes/index.js');
 
