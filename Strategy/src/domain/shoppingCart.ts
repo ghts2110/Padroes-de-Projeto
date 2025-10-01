7e601a36c3c1dfbc5f9674d86c9ea93967c98965
ghts2110 <gabrieltorres2110@gmail.com>
2025-10-01 11:31:25 -0300

    domain

---
+++ Strategy/src/domain/shoppingCart.ts
@@ -0,0 +1,29 @@
+import { CartItem, lineTotal } from "./cartItem.js";
+
+export class ShoppingCart {
+  private items: CartItem[] = [];
+
+  add(item: CartItem): void {
+    this.items.push(item);
+  }
+
+  removeById(id: string): void {
+    this.items = this.items.filter(i => i.id !== id);
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  getItems(): CartItem[] {
+    return [...this.items];
+  }
+
+  count(): number {
+    return this.items.length;
+  }
+
+  subtotal(): number {
+    return this.items.reduce((sum, i) => sum + lineTotal(i), 0);
+  }
+}
